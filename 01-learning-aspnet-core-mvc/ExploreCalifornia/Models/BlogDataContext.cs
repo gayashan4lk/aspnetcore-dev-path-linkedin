50ed72298330e8c35b0cc420dce2ccfdde397adc
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-07-07 19:32:43 +0530

    view components

--- 01-learning-aspnet-core-mvc/ExploreCalifornia/Models/BlogDataContext.cs
+++ 01-learning-aspnet-core-mvc/ExploreCalifornia/Models/BlogDataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace ExploreCalifornia.Models
 {
@@ -10,5 +11,41 @@ namespace ExploreCalifornia.Models
         }
 
         public DbSet<Post> Posts { get; set; }
+
+        public IQueryable<MonthlySpecial> MonthlySpecials
+        {
+            get
+            {
+                return new[]
+                {
+                    new MonthlySpecial
+                    {
+                        Key = "calm",
+                        Name = "California Calm Package",
+                        Type = "Day Spa Package",
+                        Price = 250,
+                    },
+                    new MonthlySpecial
+                    {
+                        Key = "desert",
+                        Name = "From Deseart to Sea",
+                        Type = "2 Day Salton Sea",
+                        Price = 350,
+                    },
+                    new MonthlySpecial {
+                        Key = "backpack",
+                        Name = "Backpack Cali",
+                        Type = "Big Sur Retreat",
+                        Price = 620,
+                    },
+                    new MonthlySpecial {
+                        Key = "taste",
+                        Name = "Taste of California",
+                        Type = "Tapas & Groves",
+                        Price = 150,
+                    },
+                }.AsQueryable();
+            }
+        }
     }
 }
---
+++ 01-learning-aspnet-core-mvc/ExploreCalifornia/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ExploreCalifornia.ViewComponents
+{
+    /*public class MonthlySpecialsViewComponent <-- Should have 'ViewComponent' suffix in the class name
+    {
+    }*/
+    // OR
+
+    /*public class MonthlySpecialsViewComponent : ViewComponent
+    {
+    }*/
+    //OR
+
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
+    {
+        private readonly BlogDataContext db;
+
+        public MonthlySpecialsViewComponent(BlogDataContext db)
+        {
+            this.db = db;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var specials = db.MonthlySpecials.ToArray();
+            return View(specials);
+        }
+    }
+}
