64907ab8db6a2ce6e6764b43cb01d645be327ec8
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-07-08 21:48:12 +0530

    create RoomsController

---
+++ 02-build-restful-api/LandonApi/Controllers/RoomsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LandonApi.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRooms))]
+        public IActionResult GetRooms()
+        {
+            return Ok("I have rooms");
+        }
+    }
+}
