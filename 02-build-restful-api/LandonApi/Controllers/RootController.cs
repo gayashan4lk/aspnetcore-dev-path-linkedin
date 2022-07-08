5846d0f88f2fc577b746a003d647b4cf0d2317ee
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-07-08 21:40:45 +0530

    root controller

---
+++ 02-build-restful-api/LandonApi/Controllers/RootController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LandonApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}
