0115ce0ce13174d972dc0704e5813a64a63321c3
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-07-10 19:06:58 +0530

    seed the database

--- 02-build-restful-api/LandonApi/Program.cs
+++ 02-build-restful-api/LandonApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +14,9 @@ namespace LandonApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            InitializeDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +25,23 @@ namespace LandonApi
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void InitializeDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the database.");
+                }
+            }
+        }
     }
 }
