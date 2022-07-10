e6aebea1893ed79ece6aa483515d37f2c30f4d45
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-07-10 19:28:01 +0530

    return a resource

--- 02-build-restful-api/LandonApi/Controllers/RoomsController.cs
+++ 02-build-restful-api/LandonApi/Controllers/RoomsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LandonApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Threading.Tasks;
 
 namespace LandonApi.Controllers
 {
@@ -7,10 +10,40 @@ namespace LandonApi.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
+        private readonly HotelApiDbContext context;
+
+        public RoomsController(HotelApiDbContext  context)
+        {
+            this.context = context;
+        }
+
         [HttpGet(Name = nameof(GetRooms))]
         public IActionResult GetRooms()
         {
             throw new NotImplementedException();
+        }
+
+        // GET  /rooms/{roomId}
+        [HttpGet("{roomId}", Name = nameof(GetRoomById))]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult<Room>> GetRoomById(Guid roomId)
+        {
+            var entity = await context.Rooms.SingleOrDefaultAsync(x => x.Id == roomId);
+
+            if(entity == null)
+            {
+                return NotFound();
+            }
+
+            var resources = new Room
+            {
+                Href = Url.Link(nameof(GetRoomById), new { roomId = entity.Id }),
+                Name = entity.Name,
+                Rate = entity.Rate / 100.0m,
+            };
+
+            return Ok(resources);
         }
     }
 }
